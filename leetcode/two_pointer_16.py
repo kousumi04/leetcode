430353e807b852b5722d2eff51960535dd154639
kousumi04 <kousumimondal2004@gmail.com>
2026-06-18 17:40:58 +0530

    explainations added

--- leetcode/two_pointer_16.py
+++ leetcode/two_pointer_16.py
@@ -1,15 +1,14 @@
 class Solution:
     def threeSumClosest(self, nums, target):
         nums.sort()
-        n=len(nums)
         res=nums[0]+nums[1]+nums[2]
         max_diff=float('inf')
-        for i in range(0, n-2):
+        for i in range(0, len(nums)-2):
             left=i+1
-            right=n-1
-            # sum=-1*nums[i]
+            right=len(nums)-1
             while left<right:
-                s=nums[i]+nums[left]+nums[right]
+                s=nums[i]+nums[left]+nums[right] # sum of first three elements
+                # best case
                 if s==target:
                     return target
                     
@@ -17,6 +16,7 @@ class Solution:
                     left+=1
                 else:
                     right-=1
+                    # finding the difference or sum from target
                 diff= abs(s-target)
                 if diff<max_diff:
                     res=s
