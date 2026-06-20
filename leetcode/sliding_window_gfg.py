cccbeeb8d11db95953459c5a637c4c0a9c3df042
kousumi04 <kousumimondal2004@gmail.com>
2026-06-20 19:36:23 +0530

    comments added

--- leetcode/sliding_window_gfg.py
+++ leetcode/sliding_window_gfg.py
@@ -5,6 +5,11 @@ class Solution:
         max_sum=sum(arr[:k])
         res=max_sum
         for i in range(k,len(arr)):
+            '''100+200=300
+               300-100+300=500
+               500-200+400=700
+               max(300, 500, 700)->700
+            '''
             max_sum+=arr[i]-arr[i-k]
             res=max(res, max_sum)
         return res     
