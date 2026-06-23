116051e88a101c1b66a064a0bcd8cad79c93d15e
kousumi04 <kousumimondal2004@gmail.com>
2026-06-23 22:42:36 +0530

    comments added

--- leetcode/two_pointer_844.py
+++ leetcode/two_pointer_844.py
@@ -1,14 +1,19 @@
 class Solution:
     def backspaceCompare(self, s, t):
-        def process(string):
-            stack=[]
-            for ch in string:
+        def process(string): 
+            '''takes a string as input and 
+            returns the final string after applying all backspaces.'''
+            stack=[]  #empty list
+            for ch in string: #for each ch in s or t
                 if ch=="#":
-                    if stack:
+                    if stack: # if found then pop
                         stack.pop()
                 else:
-                    stack.append(ch)
+                    stack.append(ch) #[bxj]
+                    # after '#' is found, the last element in the stack get appended
             return "".join(stack)
+        '''takes all elements of a list of strings and 
+        combines them into a single string with no space.'''
         return process(s)==process(t)                 
 sol=Solution()
 s="bxj##tw"
