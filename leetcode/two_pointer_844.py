108a968af7aa9e1fc96a2bde439fe91e339312ff
kousumi04 <kousumimondal2004@gmail.com>
2026-06-23 22:13:25 +0530

    backspace compare done

---
+++ leetcode/two_pointer_844.py
@@ -0,0 +1,18 @@
+class Solution:
+    def backspaceCompare(self, s, t):
+        def process(string):
+            stack=[]
+            for ch in string:
+                if ch=="#":
+                    if stack:
+                        stack.pop()
+                else:
+                    stack.append(ch)
+            return "".join(stack)
+        return process(s)==process(t)                 
+sol=Solution()
+s="bxj##tw"
+t="bxo#j##tw"
+print(sol.backspaceCompare(s,t))
+
+        
