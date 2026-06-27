d378c98dc30bf2d8c1354a378cbeee44bdfceec8
kousumi04 <kousumimondal2004@gmail.com>
2026-06-27 14:45:04 +0530

    longest ones done

---
+++ leetcode/sliding_window_1004.py
@@ -0,0 +1,20 @@
+class Solution:
+    def longestOnes(self, nums, k):
+
+        res=0
+        left=0
+        zeros=0
+        for right in range(len(nums)):
+            if nums[right]==0:
+                zeros+=1 
+                #increments the count of zeros if present
+            while zeros>k:
+                if nums[left]==0:
+                    zeros-=1
+                left+=1    
+            res=max(res, right-left+1)
+        return res
+s=Solution()
+nums=[0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1]
+k=3
+print(s.longestOnes(nums, k))
