eb50440288da670859b98bb65ce5f834699be6c9
kousumi04 <kousumimondal2004@gmail.com>
2026-06-27 11:25:26 +0530

    longest substring after replacement done

---
+++ leetcode/sliding_window_424.py
@@ -0,0 +1,44 @@
+# class Solution:
+#     def characterReplacement(self, s, k):
+#         count={}
+#         res=0
+#         left=0
+
+#         for right in range(len(s)):
+#             count[s[right]]=count.get(s[right],0)+1
+            
+#             while (right-left+1)-max(count.values())>k:
+#                 count[s[left]]-=1
+#                 l+=1
+#             res=max(res, right-left+1)
+#         return res    
+
+# sol=Solution()
+# s="ABAB"
+# k=4
+# print(sol.characterReplacement(s,k))   
+
+
+
+# mII-
+
+class Solution:
+    def characterReplacement(self, s, k):
+        count={}
+        res=0
+        left=0
+        maxf=0
+        for right in range(len(s)):
+            count[s[right]]=count.get(s[right],0)+1
+            maxf=max(maxf, count[s[right]])
+            while (right-left+1)-maxf>k:
+                count[s[left]]-=1
+                left+=1
+            res=max(res, right-left+1)
+        return res    
+
+sol=Solution()
+s="ABAB"
+k=4
+print(sol.characterReplacement(s,k))  
+
