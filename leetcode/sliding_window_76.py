d7875e14b6a47ef3908a74af697aa0c3ca2a7da9
kousumi04 <kousumimondal2004@gmail.com>
2026-06-28 15:26:01 +0530

    76 done

---
+++ leetcode/sliding_window_76.py
@@ -0,0 +1,32 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if t=="":
+            return ""
+        countT, window={},{}
+        for ch in t:
+            countT[ch]= countT.get(ch,0)+1
+        have, need=0, len(countT)  
+        res, resLen=[-1,-1], float("inf")
+        l=0
+        for r in range(len(s)):
+            ch=s[r]
+            window[ch]=window.get(ch,0)+1
+            if ch in countT and window[ch]==countT[ch]:
+                have+=1
+            while have==need:
+                #update result 
+                if (r-l+1)<resLen:
+                    res=[l,r]
+                    resLen=r-l+1
+                    # pop from left in current window
+                window[s[l]]-=1
+                if s[l] in countT and window [s[l]]< countT[s[l]]:
+                    have-=1
+                l+=1  
+        l, r = res
+        return s[l:r+1] if resLen != float("inf") else ""          
+
+sol=Solution()
+s = "ADOBECODEBANC"
+t = "ABC"
+print(sol.minWindow(s,t))
