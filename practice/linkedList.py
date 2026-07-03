8de239d813fa1f8f6c67887ab0e42798ee543144
kousumi04 <kousumimondal2004@gmail.com>
2026-07-03 17:29:59 +0530

    insertion operations done

--- practice/linkedList.py
+++ practice/linkedList.py
@@ -2,14 +2,42 @@ class Node:
     def __init__(self, data):
         self.data=data
         self.next=None
-a=Node(10)
-a.next=Node(20)
-a.next.next=Node(30)
-a.next.next.next=Node(40)
-cur=a
-# traverse a linkedlist
-while cur:
-    print(cur.data, end="->")
-    cur=cur.next
-print("None")    
 
+# traverse a linkedlist
+def printLinkedList(head):
+    cur=head
+    while cur!=None:
+        print(cur.data, end="->")
+        cur=cur.next
+    print("None")   
+
+head=Node(10)
+head.next=Node(20)
+head.next.next=Node(30)
+head.next.next.next=Node(40)
+
+#insertion at the beginning  
+newNode=Node(50)
+newNode.next=head
+head= newNode
+
+# insertion at the end
+newNode=Node(60)
+cur=head
+while cur.next!=None:
+    cur=cur.next
+cur.next=newNode    
+
+# insertion at the kth index
+k=2
+newNode=Node(70)
+cur=head
+for i in range(k-1):
+    cur=cur.next
+newNode.next=cur.next
+cur.next=newNode    
+
+# delete the first node
+
+    
+printLinkedList(head)
