f7f67f8c1f90a1990add2d586b63ba182a57fcee
kousumi04 <kousumimondal2004@gmail.com>
2026-07-04 16:21:06 +0530

    delete duplicates from a linkedlist

--- leetcode/ll_83.py
+++ leetcode/ll_83.py
@@ -3,10 +3,16 @@ class Solution:
         self.val = val
         self.next = next
     def deleteDuplicates(self, head):
-        slow=head
-        fast=head
-        res=[]
-           
+        cur=head
+        # edge cases
+        if head==None or head.next==None:
+            return head
+        while cur!=None and cur.next!=None:
+            if cur.next.val==cur.val:
+                cur.next=cur.next.next
+            else:
+                cur=cur.next
+        return head    
 head=Solution(1)
 head.next=Solution(1)
 head.next.next=Solution(2)
@@ -14,7 +20,7 @@ head.next.next.next=Solution(3)
 head.next.next.next.next=Solution(3)
 s=Solution()
 new_head=s.deleteDuplicates(head)
-curr = new_head
+curr = head
 while curr:
     print(curr.val, end=" ")
     curr = curr.next    
