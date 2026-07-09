3e748a59cb930a65cd38f7b47025c0a3425f86f4
kousumi04 <kousumimondal2004@gmail.com>
2026-07-09 22:08:32 +0530

    ma product  subarray done

--- leetcode/kadanes_152.py
+++ leetcode/kadanes_152.py
@@ -1,15 +1,17 @@
-# class Solution:
-#     def maxProduct(self, nums):
-#         i=0
-#         best=nums[i]
-#         low=nums[i]
-#         ans=nums[i]
-#         for i in range(1, len(nums)):
-#             v1=best*nums[i]
-#             v2=nums[i]
-#             best=max(v1,v2)
-#             ans=max(ans, best)
-#         return ans    
-# s=Solution()
-# nums=[-2,3,-4]
-# print(s.maxProduct(nums))
+class Solution:
+    def maxProduct(self, nums):
+        i=0
+        maxend=nums[i]
+        minend=nums[i]
+        ans=nums[i]
+        for i in range(1, len(nums)):
+            v1=nums[i]
+            v2=minend*nums[i]
+            v3=maxend*nums[i]
+            maxend=max(v1,max(v2,v3))
+            minend=min(v1,min(v2,v3))
+            ans=max(ans, max(maxend,minend))
+        return ans    
+s=Solution()
+nums=[-2,3,-4]
+print(s.maxProduct(nums))
