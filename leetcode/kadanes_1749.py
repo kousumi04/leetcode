2a6d68ef06b6719f4b8c6fb3011dd0f579633f6e
kousumi04 <kousumimondal2004@gmail.com>
2026-07-10 22:46:53 +0530

    max absolute sum using kadanes's pattern

---
+++ leetcode/kadanes_1749.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxAbsoluteSum(self, nums):
+        i=0
+        minend=nums[i]
+        maxend=nums[i]
+        res=abs(nums[i])
+        for i in range(1, len(nums)):
+            v1=nums[i]
+            v2=minend+nums[i]
+            v3=maxend+nums[i]
+            maxend=max(v1,v3)
+            minend=min(v1,v2)
+            res=max(res, abs(maxend), abs(minend))
+        return res    
+    
+s=Solution()
+nums=[2,-5,1,-4,3,-2]
+print(s.maxAbsoluteSum(nums))
