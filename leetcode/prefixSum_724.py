2e538b27c3b05cd2d06da7a3eb6a73ef06b1ccfa
kousumi04 <kousumimondal2004@gmail.com>
2026-07-11 23:52:33 +0530

    find pivot index done

---
+++ leetcode/prefixSum_724.py
@@ -0,0 +1,13 @@
+class Solution:
+    def pivotIndex(self, nums):
+        total=sum(nums)
+        left=0
+        for i in range(len(nums)):
+            right=total-left-nums[i]
+            if left==right:
+                return i
+            left+=nums[i]
+        return -1    
+s=Solution()
+nums=[1,7,3,6,5,6]
+print(s.pivotIndex(nums))  
