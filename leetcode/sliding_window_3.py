882d710e73ee6dcc02532a11c61a506e8318f35f
kousumi04 <kousumimondal2004@gmail.com>
2026-07-14 12:07:25 +0530

    practice

--- leetcode/sliding_window_3.py
+++ leetcode/sliding_window_3.py
@@ -4,12 +4,12 @@ class Solution:
         res=0
         freq=set()
         for high in range(len(s)):
-            while s[high] in freq: #duplicate check
+            while s[high] in freq:
                 freq.remove(s[low])
                 low+=1
             freq.add(s[high])
-            res=max(res, high-low+1)
-        return res    
+            res=max(res, high-low+1)  
+        return res      
 sol=Solution()
 s="abcabcbb"
 print(sol.lengthOfLongestSubstring(s))
--- leetcode/sliding_window_424.py
+++ leetcode/sliding_window_424.py
@@ -24,10 +24,10 @@
 
 class Solution:
     def characterReplacement(self, s, k):
-        count={}
-        res=0
         left=0
-        maxf=0
+        res=0
+        maxf=0        
+        count={}
         for right in range(len(s)):
             count[s[right]]=count.get(s[right],0)+1
             maxf=max(maxf, count[s[right]])
@@ -35,10 +35,9 @@ class Solution:
                 count[s[left]]-=1
                 left+=1
             res=max(res, right-left+1)
-        return res    
+        return res
 
 sol=Solution()
 s="ABAB"
 k=4
 print(sol.characterReplacement(s,k))  
-
