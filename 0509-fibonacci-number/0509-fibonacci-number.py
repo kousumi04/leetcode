bce0f3236a88b8d473d40a066da0e755574612d6
Kousumi Mondal <kousumimondal2004@gmail.com>
2026-07-21 12:20:42 +0530

    Time: 351 ms (5.06%), Space: 19.2 MB (56.83%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        if n==0:
+            return 0
+        elif n==1:
+            return 1
+        else:    
+            ans1=self.fib(n-1)
+            ans2=self.fib(n-2)
+        return ans1+ans2    
