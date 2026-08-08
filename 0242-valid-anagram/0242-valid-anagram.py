8ccf043a3bbf8c2b62afb875ced98069bdd493dd
Kousumi Mondal <kousumimondal2004@gmail.com>
2026-08-08 16:01:29 +0530

    Time: 19 ms (16.47%), Space: 19.3 MB (75.77%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,16 +1,12 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s)!=len(t):
-            return False
-        s=list(s) 
-        t=list(t)
-        s.sort()
-        t.sort()      
-        if s==t:
-            return True
-        else:
-            return False
-
-
-#top otp
-#['t','o','p'] = [o,p,t]
+        # if len(s)>len(t):
+        #     return []
+        countS =Counter(s)
+        for char in t:
+            if char not in countS:
+                return False
+            countS[char]-=1
+            if countS[char]==0:
+                del countS[char]    
+        return len(countS)==0    
