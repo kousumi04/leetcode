644bfbc0a02301451fa6cb9833c4eb3c945b9255
kousumi04 <kousumimondal2004@gmail.com>
2026-08-10 22:36:15 +0530

    count of elements greater than than the prior elements

--- tcs/greaterPrior.py
+++ tcs/greaterPrior.py
@@ -1,8 +1,20 @@
+# n=int(input())
+# arr=list(map(int, input().split()))
+# i=1
+# freq=0
+# for i in range(len(arr)):
+#     if arr[i-1]>=arr[i]:
+#         freq+=1
+# print(freq)        
+
+
+
 n=int(input())
 arr=list(map(int, input().split()))
-i=1
-freq=0
+maximum=arr[0]
+freq=1
 for i in range(len(arr)):
-    if arr[i-1]>=arr[i]:
+    if maximum<arr[i]:
         freq+=1
+        maximum=arr[i]
 print(freq)        
