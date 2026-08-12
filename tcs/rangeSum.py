17b18ff9cf812812b9be4336d1aa044e9188a1b0
kousumi04 <kousumimondal2004@gmail.com>
2026-08-12 10:34:41 +0530

    range sum2 done

--- tcs/rangeSum.py
+++ tcs/rangeSum.py
@@ -1,7 +1,18 @@
+# i=int(input())
+# j=int(input())
+# if i>=j or i<0 or j>=10000:
+#     print("Invalid input i & j.")
+# else:
+#     s=(j*(j+1)/2)-(i*(i-1)/2)
+# print(s)   
+# 
+
 i=int(input())
 j=int(input())
+s=0
 if i>=j or i<0 or j>=10000:
     print("Invalid input i & j.")
-else:
-    s=(j*(j+1)/2)-(i*(i-1)/2)
+for k in range(i, j+1):
+    s+=k
 print(s)    
+
