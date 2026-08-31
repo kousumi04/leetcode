795f3ad880383f2005784d91eaf4991edc2465e7
Kousumi Mondal <kousumimondal2004@gmail.com>
2026-08-31 20:31:39 +0530

    Time: 1082 ms (28.65%), Space: 68.8 MB (24.46%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -7,29 +7,28 @@ class WordDictionary:
         self.root=TrieNode()
     def addWord(self, word: str) -> None:
         node=self.root
-        for char in word:
-            if char not in node.children:
-                node.children[char]=TrieNode()    
-            node=node.children[char]
-        node.is_end=True
-    def search(self, word: str) -> bool:
-        def dfs(node, i):
-            if i==len(word):
-                return node.is_end
-
-            char=word[i]
-            if char !=".":
-                if char not in node.children:
-                    return False
-                node=node.children[char]
-                return dfs(node, i+1)
-            else:
-                for child in node.children.values():
-                    if dfs(child, i+1):
-                        return True
-                return False
-        return dfs(self.root, 0)                            
-
+        for c in word:
+            if c not in node.children:
+                node.children[c]=TrieNode()
+            node=node.children[c]
+        node.is_end=True    
+    def search(self, word: str) -> bool:   
+        def dfs(j, root):                       
+            node=root
+            for i in range(j, len(word)):
+                c=word[i]
+                if c==".":
+                    for child in node.children.values():
+                        if dfs(i+1, child):
+                            return True
+                    return False    
+                else:
+                    if c not in node.children:
+                        return False
+                    node=node.children[c]    
+            return node.is_end
+        return dfs(0, self.root)            
+                
 
 # Your WordDictionary object will be instantiated and called as such:
 # obj = WordDictionary()
