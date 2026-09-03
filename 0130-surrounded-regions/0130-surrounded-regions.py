f0834cb983d5bafaf04d3b1569f92c3bcb1dd040
Kousumi Mondal <kousumimondal2004@gmail.com>
2026-09-03 10:51:30 +0530

    Time: 2 ms (87.24%), Space: 22.2 MB (78.02%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -14,12 +14,35 @@ class Solution:
     def solve(self, board: List[List[str]]) -> None:
         rows, cols=len(board), len(board[0])
         visited=[[0 for _ in range(cols)] for _ in range(rows)]
+        # upper row
+        r=0
+        c=0,0
+        for c in range(cols):
+            if board[r][c]=="O":
+                if visited[r][c]==0:
+                    self.dfs(r,c,visited,rows, cols, board)
+        # last row
+        r=rows-1
+        c=0,0
+        for c in range(cols):
+            if board[r][c]=="O":
+                if visited[r][c]==0:
+                    self.dfs(r,c,visited,rows, cols, board)
+        # first col
+        r=0
+        c=0
         for r in range(rows):
-            for c in range(cols):
-                if r==0 or c==0 or r==rows-1 or c==cols-1:
-                    if board[r][c]=="O":
-                        visited[r][c]=0
-                        self.dfs(r,c,visited,rows, cols, board)
+            if board[r][c]=="O":
+                if visited[r][c]==0:
+                    self.dfs(r,c,visited,rows, cols, board)
+        # last col
+        r=0
+        c=cols-1
+        for r in range(rows):
+            if board[r][c]=="O":
+                if visited[r][c]==0:
+                    self.dfs(r,c,visited,rows, cols, board)            
+
         for r in range(rows):
             for c in range(cols):
                 if board[r][c]=="O" and visited[r][c]==0:
