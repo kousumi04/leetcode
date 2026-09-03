aca66754fe4798e5053bd54773d64d38f6969630
Kousumi Mondal <kousumimondal2004@gmail.com>
2026-09-03 20:36:20 +0530

    Time: 0 ms (100%), Space: 20.4 MB (22.55%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.py
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -1,11 +1,13 @@
 class Solution:
+       
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-        visited=set()
-        stack=[0]
-        while stack:
-            room=stack.pop()
-            visited.add(room)
+        visited=[0 for _ in range(len(rooms))]
+        def dfs( room):
+            if room<0:
+                return
+            visited[room]=1
             for key in rooms[room]:
-                if key not in visited:
-                    stack.append(key)
-        return len(visited)==len(rooms)            
+                if visited[key]==0:
+                    dfs(key) 
+        dfs(0)
+        return sum(visited)==len(rooms)            
