f35d24df8cd3ba6d623ba568efaacef3772cdb29
Kousumi Mondal <kousumimondal2004@gmail.com>
2026-09-06 19:20:28 +0530

    Time: 3 ms (85.32%), Space: 21.2 MB (47.51%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,10 +1,12 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        freq={}
-        res, majority=0,0
-        for i in nums:
-            freq[i]=freq.get(i,0)+1
-            if freq[i]>majority:
-                res=i
-                majority=freq[i]
-        return res         
+        # freq={}
+        # res, majority=0,0
+        # for i in nums:
+        #     freq[i]=freq.get(i,0)+1
+        #     if freq[i]>majority:
+        #         res=i
+        #         majority=freq[i]
+        # return res    
+        c=Counter(nums)
+        return c.most_common(1)[0][0]     
