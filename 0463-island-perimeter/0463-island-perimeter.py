6c453a75452a59944e582f45121305b28d0d6412
Kousumi Mondal <kousumimondal2004@gmail.com>
2026-09-08 10:07:34 +0530

    Time: 31 ms (77.03%), Space: 19.7 MB (51.17%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -1,28 +1,14 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int: 
-        grid_copy=deepcopy(grid)
-        rows=len(grid_copy)
-        cols=len(grid_copy[0])
+        rows=len(grid)
+        cols=len(grid[0])
         perimeter=0
-        queue=deque()
         for r in range(rows):
             for c in range(cols):
-                if grid_copy[r][c]==1:
-                    queue.append((r,c))
-                    grid_copy[r][c]=2
-                    break
-            if queue:
-                break        
-        while len(queue)!=0:
-            i,j=queue.popleft()
-            for x, y in [(-1,0),(1,0),(0,-1),(0,1)]:
-                ni, nj=i+x, j+y
-                if ni<0 or ni>=rows or nj<0 or nj>=cols:
-                    perimeter+=1
-                else:    
-                    if grid_copy[ni][nj]==0:
-                        perimeter+=1
-                    elif grid_copy[ni][nj]==1:
-                        queue.append((ni, nj))
-                        grid_copy[ni][nj]=2  
-        return perimeter        
+                if grid[r][c]==1:
+                    perimeter+=4
+                    if r>0 and grid[r-1][c]==1:
+                        perimeter-=2      
+                    if c>0 and grid[r][c-1]==1:
+                        perimeter-=2
+        return perimeter
