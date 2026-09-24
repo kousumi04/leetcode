78c01873dd136252b13413e266512ad4d5550920
Kousumi Mondal <kousumimondal2004@gmail.com>
2026-09-24 21:51:55 +0530

    Time: 21 ms (6.19%), Space: 20.5 MB (9.95%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,13 +1,13 @@
 class Solution:
-
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s)!=len(t):
-            return False
-        s=list(s) 
-        t=list(t)
-        s.sort()
-        t.sort() 
-        if s==t:
-            return True
-        else:
-            return False 
+        # if len(s)!=len(t):
+        #     return False
+        # s=list(s) 
+        # t=list(t)
+        # s.sort()
+        # t.sort() 
+        # if s==t:
+        #     return True
+        # else:
+        #     return False 
+        return (sorted(s)==sorted(t))
